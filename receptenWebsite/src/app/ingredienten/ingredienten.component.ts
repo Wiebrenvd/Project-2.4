925abc6b0b709982b05c93caf4be865452a5aade
Wiebrenvd <34299279+Wiebrenvd@users.noreply.github.com>
2020-05-27 16:46:10 +0100

    ingredienten (add)

--- receptenWebsite/src/app/ingredienten/ingredienten.component.ts
+++ receptenWebsite/src/app/ingredienten/ingredienten.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from "./ingredienten";
 
 @Component({
   selector: 'app-ingredienten',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingredienten.component.css']
 })
 export class IngredientenComponent implements OnInit {
+  ingredienten = [
+    new Ingredient('Appels', '700g'),
+    new Ingredient('Bloem', '500g'),
+    new Ingredient('boter', '400g'),
+    new Ingredient('eiren', '2')
+  ];
 
   constructor() { }
 
