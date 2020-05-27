f47fcdae0db1679ce98ca158ad2312d8ed1fb7fa
Tim Perdok <Onmogeloos@users.noreply.github.com>
2020-05-27 16:42:47 +0200

    [add] Homepage

---
+++ receptenWebsite/src/app/populaire-recepten/populaire-recepten.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-populaire-recepten',
+  templateUrl: './populaire-recepten.component.html',
+  styleUrls: ['./populaire-recepten.component.css']
+})
+export class PopulaireReceptenComponent implements OnInit {
+  popularRecipes: string[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.popularRecipes = ['Rijst', 'kip', 'spaghetti'];
+  }
+
+}
---
+++ receptenWebsite/src/app/recepten-in-huis/recepten-in-huis.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recepten-in-huis',
+  templateUrl: './recepten-in-huis.component.html',
+  styleUrls: ['./recepten-in-huis.component.css']
+})
+export class ReceptenInHuisComponent implements OnInit {
+  recipes: string[];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.recipes = ['Rijst', 'kip', 'spaghetti'];
+  }
+
+}
