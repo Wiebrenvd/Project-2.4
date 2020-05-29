c6204bcc577e815bbb02d202b8c03277e69c120e
Wiebrenvd <34299279+Wiebrenvd@users.noreply.github.com>
2020-05-29 12:11:12 +0100

    less hardcoded values

--- receptenWebsite/src/app/mock-ingredienten.ts
+++ receptenWebsite/src/app/mock-ingredienten.ts
@@ -9,3 +9,7 @@ export const ingredienten = [
   new Ingredient('appels', '8'),
   new Ingredient('eetlepel suiker', '1')
 ];
+export const bereidwijze = 'Verwarm de oven voor tot 180 °C. Maak een deeg door bloem, boter, suiker en eieren goed door elkaar te kneden. Laat het deeg even rusten.\n' +
+  '\n' + 'Schil de appels en haal de klokhuizen eruit. Snijd de appels in blokjes en meng die met de suiker en de kaneel.\n' +
+  '\n' + 'Rol het deeg uit en leg het in een beboterde taartvorm. Schep de vulling hierin en druk goed aan. Gebruik restjes deeg om de bovenkant van de taart te versieren.\n' +
+  '\n' + 'Bak de taart in de voorverwarmde oven ongeveer 50 minuten tot gaar en goudbruin.';
